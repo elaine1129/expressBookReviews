1d6c8e8a51a8aec4e33783364d81375db5ad5808
elaine1129 <elainelowjingyi00@gmail.com>
2025-10-15 04:33:24 -0400

    login

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,16 +5,21 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 public_users.post("/register", (req,res) => {
-    const username = req.params.username;
-    const password = req.params.password;
+    const username = req.body.username;
+    const password = req.body.password;
 
     if (username && password){
+        if(isValid(username)){
+            users.push({"username": username, "password": password});
+            res.status(200).json({message: "User successfully registered"});
+        } else {
+            res.status(404).json({message: "User already exists!"});
+        }
+
 
     } else {
         res.status(404).json({message: "Unable to register user!"});
     }
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get the book list available in the shop
